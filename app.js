0c5252096e43b9a93c9749992ee94f7014ee4b50
Ashish <120723880+Mr-AshishBhandari@users.noreply.github.com>
2026-05-05 15:05:15 +0545

    Add CORS support to the Express app

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ import express  from 'express';
 import mongoose  from "mongoose";
 import medicineRoute from './routes/medicine.js';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 
 const app = express()
 app.use(express.json());
+app.use(cors());
 dotenv.config()
 
 app.get('/',(req,res)=>{
