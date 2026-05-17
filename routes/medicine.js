f3f005fc44224363348a41e3067425ad7d9a7e70
Ashish <120723880+Mr-AshishBhandari@users.noreply.github.com>
2026-05-17 18:55:46 +0545

    Increase Fuse search threshold from 0.95 to 0.99

--- routes/medicine.js
+++ routes/medicine.js
@@ -32,7 +32,7 @@ router.get('/:generic_name',async (req, res)=>{
 
     const fuse = new Fuse(medicines, {
       keys: ["generic_name"],
-      threshold: 0.95, 
+      threshold: 0.99, 
       includeScore: true,
     });
     const result = fuse.search(generic_name)
